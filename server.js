5e3357d39e7c26fed18ded497aa3fe63d4d806b3
Тимофей Трофимов <t.trofimov@just-work.org>
2021-08-07 09:57:41 +0300

    Request Header Parser Microservice

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('accept-language'),
+    software: req.get('user-agent'),
+  });
 });
 
 
